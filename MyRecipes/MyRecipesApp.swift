7af955bfd292cb0bd4615151aa6ff418aa741bda
SarvarBek <ssarvar507@gmail.com>
2025-05-11 16:51:47 +0500

    initial commit

---
+++ MyRecipes/MyRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRecipesApp.swift
+//  MyRecipes
+//
+//  Created by Sarvar Boltaboyev on 11/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MyRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
